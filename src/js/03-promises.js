8f29ea8a55c3516d3a8b2c6d7994cd1a2c9a106c
ValentinaDej <safonikv@gail.com>
2022-11-21 22:16:18 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -18,39 +18,40 @@ function onSubmit(evt) {
   const options = {
     delayStart: refs.delayEl.value,
     //  amount: refs.amountEl.value,
+    amount: 5,
     step: refs.stepEl.value,
   };
-  // createPromiseStack(options);
+  createPromiseStack(options);
 }
 
-// function createPromiseStack({ delayStart, amount, step }) {
-//   for (let position = 1; position <= amount; position += 1) {
-//     delay = Number(delayStart) + Number(step) * (Number(position) - 1);
-//     createPromise(position, delay)
-//       .then(({ position, delay }) => {
-//         Notiflix.Notify.success(
-//           `✅ Fulfilled promise ${position} in ${delay}ms`
-//         );
-//       })
-//       .catch(({ position, delay }) => {
-//         Notiflix.Notify.failure(
-//           `❌ Rejected promise ${position} in ${delay}ms`
-//         );
-//       });
-//   }
-// }
+function createPromiseStack({ delayStart, amount, step }) {
+  for (let position = 1; position <= amount; position += 1) {
+    delay = Number(delayStart) + Number(step) * (Number(position) - 1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+}
 
-// function createPromise(position, delay) {
-//   return new Promise((resolve, reject) => {
-//     const shouldResolve = Math.random() > 0.3;
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//         resolve({ position, delay });
-//       } else {
-//         reject({ position, delay });
-//       }
-//     }, delay);
-//   });
-// }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
 
 refs.formEl.addEventListener('submit', onSubmit);

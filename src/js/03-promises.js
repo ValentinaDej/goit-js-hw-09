e4d97f6f25734f8428c687279b4183f7de01c93f
ValentinaDej <safonikv@gail.com>
2022-11-21 21:55:23 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,55 +1,55 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-const refs = {
-  formEl: document.querySelector('.form'),
-  delayEl: document.querySelector('input[name="delay"]'),
-  stepEl: document.querySelector('input[name="step"]'),
-  amountEl: document.querySelecstor('input[name="amount"]'),
-};
-
 Notiflix.Notify.init({
   timeout: 2000,
   clickToClose: true,
 });
 
-function onSubmit(evt) {
-  evt.preventDefault();
-  const options = {
-    delayStart: refs.delayEl.value,
-    step: refs.stepEl.value,
-  };
-  createPromiseStack(options);
-}
+// const refs = {
+//   formEl: document.querySelector('.form'),
+//   delayEl: document.querySelector('input[name="delay"]'),
+//   stepEl: document.querySelector('input[name="step"]'),
+//   amountEl: document.querySelecstor('input[name="amount"]'),
+// };
 
-function createPromiseStack({ delayStart, amount, step }) {
-  for (let position = 1; position <= amount; position += 1) {
-    delay = Number(delayStart) + Number(step) * (Number(position) - 1);
-    createPromise(position, delay)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
-        );
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(
-          `❌ Rejected promise ${position} in ${delay}ms`
-        );
-      });
-  }
-}
+// function onSubmit(evt) {
+//   evt.preventDefault();
+//   const options = {
+//     delayStart: refs.delayEl.value,
+//     step: refs.stepEl.value,
+//   };
+//   createPromiseStack(options);
+// }
 
-function createPromise(position, delay) {
-  return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-    setTimeout(() => {
-      if (shouldResolve) {
-        resolve({ position, delay });
-      } else {
-        reject({ position, delay });
-      }
-    }, delay);
-  });
-}
+// function createPromiseStack({ delayStart, amount, step }) {
+//   for (let position = 1; position <= amount; position += 1) {
+//     delay = Number(delayStart) + Number(step) * (Number(position) - 1);
+//     createPromise(position, delay)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(
+//           `❌ Rejected promise ${position} in ${delay}ms`
+//         );
+//       });
+//   }
+// }
 
-refs.formEl.addEventListener('submit', onSubmit);
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// refs.formEl.addEventListener('submit', onSubmit);
